58b1e217b791113ab0970750a0e109d0b74cc248
Jesus Alvarado <112035392+jesusas99@users.noreply.github.com>
2022-08-24 19:43:15 -0700

    Update HeroCard.jsx

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
-    const heroImageUrl = `./assets/heroes/${ id }.jpg`;
+    const heroImageUrl = `../../../assets/heroes/${ id }.jpg`;
 
   return (
     <div className="col ">
